a9f80ccda5177879f3e872ab09f61ea2f00589be
reynolds-nate <73624737+reynolds-nate@users.noreply.github.com>
2020-11-17 18:40:37 -0500

    fxn definitions
    adding place holder function names and their requirements

--- hangman.py
+++ hangman.py
@@ -1,13 +1,44 @@
 from random import randint
-
+# Getting a list of letters and words from preset text files
+#all words and letters are put into lists as lowercase
 f = open('hangmanWords.txt')
 wordList = f.read().splitlines()
 
 f2 = open('alphabet.txt')
 alphabet = f2.read().splitlines()
 
-for i in range(26):
-    alphabet[i] = alphabet[i].upper()
+#SHOW WORD PROGRESS
+# Each round the word is displayed (e.g. Word: __mm_t)
+# Guessed letters are unmasked.
+# Hidden letters are shown as underscores.
+
+#SHOW REMAINING GUESSES
+# Each round display the number of chances remaining.
+# Display Chances remaining: CHANCES where CHANCES is an integer.
+
+#GUESS LETTER OR WORD
+# When a player enters a single character, it guesses a letter.
+# When a player enters more than a single character, it guesses the word.
+
+#CHECK GUESSED LETTERS
+# If the guessed letter is within the word, those letters are unveiled.
+# Display the number of times the letter is found when it is present.
+# If the guessed letter is not within the word, subtract one from the chances remaining.
+# The user is alerted if the guess is not found.
+# If the guessed letter was already submitted, do not decrement the chances remaining.
+# Display an error message if the letter was already guessed.
+
+#CHECK GUESSED WORD
+# If the guessed word matches the hidden word, the player wins the game.
+# Output a congratulatory message if the guess was correct.
+# If the guessed word does not match the hidden word, the player loses the game.
+# Output an apologetic message if the guess was incorrect.
+
+#GAME OVER WHEN NO MORE CHANCES
+# The game ends when the number of chances remaining reaches zero.
+# The user is alerted that they have run out of guesses
+
+
 
 numGuess=0 #number of guesses made
 guessedLetters = [] #letters that have been guessed
@@ -43,10 +74,11 @@ def getLetter():
             return currentGuess
 
 
-def updatePartial(letter):
-    if letter is in secret:
+def updatePartial(letter, guessPartial):
+    if letter in secret:
         guessPartial[secret.index(letter)]
         secretWord = selectWord(wordList)
+
 secret=list(secretWord)
 
 print(secretWord)
